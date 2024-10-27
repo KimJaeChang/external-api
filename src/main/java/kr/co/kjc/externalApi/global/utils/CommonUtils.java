5e510a0083c5149427f2922d472c804911a08cbc
wockd3325 <wockd3325@gmail.com>
2024-10-27 17:25:29 +0900

    initial commit

---
+++ src/main/java/kr/co/kjc/externalApi/global/utils/CommonUtils.java
@@ -0,0 +1,28 @@
+package kr.co.kjc.externalApi.global.utils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+import kr.co.kjc.externalApi.global.constants.CommonConstants;
+import lombok.experimental.UtilityClass;
+import org.springframework.web.multipart.MultipartFile;
+
+@UtilityClass
+public class CommonUtils {
+
+  public static File convertFile(MultipartFile multipartFile) throws IOException {
+    String ext = multipartFile.getOriginalFilename()
+        .substring(multipartFile.getOriginalFilename().lastIndexOf('.'));
+    String saveFileName = UUID.randomUUID().toString().replaceAll("-", "") + ext;
+
+    File file = new File(System.getProperty("user.dir") + saveFileName);
+    multipartFile.transferTo(file);
+
+    return file;
+  }
+
+  public static String replaceBearer(String token) {
+    return token.replace(CommonConstants.REQ_HEADER_KEY_AUTH_TOKEN_TYPE, "").trim();
+  }
+
+}
